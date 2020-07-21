f5cfe27c905be1b487c880d4f7306c1d17f8729c
Kyle <kylechess8@gmail.com>
2020-07-21 11:01:41 -0700

    Added null_core item

--- Technulligy/src/main/java/com/example/technulligy/init/ItemInit.java
+++ Technulligy/src/main/java/com/example/technulligy/init/ItemInit.java
@@ -2,10 +2,23 @@ package com.example.technulligy.init;
 
 import com.example.technulligy.Technulligy;
 
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import.net.minecraftforge.fmml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.registries.ObjectHolder;
 
-@Mod.EventBusSubscriber(modid= Technulligy.MOD_ID, bus = Bus.Mod)
+@Mod.EventBusSubscriber(modid = Technulligy.MOD_ID, bus = Bus.MOD)
+@ObjectHolder(Technulligy.MOD_ID)
 public class ItemInit {
 
+
+	public static Item null_core = null;
+
+	@SubscribeEvent
+	public static void registerItems(final RegistryEvent.Register<Item> event) {
+		event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("null_core"));
+	}
 }
