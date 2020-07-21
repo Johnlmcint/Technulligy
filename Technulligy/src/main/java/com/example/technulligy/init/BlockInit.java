35a8c0607c4208f576b3933ead6d53e740eb3f28
Johnlmcint <60750709+Johnlmcint@users.noreply.github.com>
2020-07-21 11:07:55 -0700

    Revert "Creating some basic initializations for Blocks"
    This reverts commit 92056e130f57af53986d9a6568936ac605151faf.

--- Technulligy/src/main/java/com/example/technulligy/init/BlockInit.java
+++ Technulligy/src/main/java/com/example/technulligy/init/BlockInit.java
@@ -6,11 +6,4 @@ import net.minecraftforge.fmml.common.Mod.EventBusSubscriber.Bus;
 
 public class BlockInit {
 
-	private static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
-
-	public static final RegistryObject<Block> ROCK_BLOCK = BLOCKS.register("rock", () -> new Block(Block.Properties.create(Material.ROCK)));
-
-	public Technulligy() {
-	    BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
-	}
 }
