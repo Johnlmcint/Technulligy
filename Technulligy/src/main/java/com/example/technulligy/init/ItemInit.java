35ac3fb8dc9fd1882566450694acb2e46217b434
Johnlmcint <60750709+Johnlmcint@users.noreply.github.com>
2020-07-21 14:12:31 -0700

    Added Some new core items to the mod

--- Technulligy/src/main/java/com/example/technulligy/init/ItemInit.java
+++ Technulligy/src/main/java/com/example/technulligy/init/ItemInit.java
@@ -17,13 +17,21 @@ public class ItemInit {
 	public static Item null_core = null;
 	public static Item aether = null;	
 	public static Item c_ore = null;
+	public static Item basic_core = null;
+	public static Item weak_core = null;
 
 	@SubscribeEvent
 	public static void registerItems(final RegistryEvent.Register<Item> event) {
-		event.getRegistry()
-				.register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("null_core"));
+		
+		event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("null_core"));
+	
 		event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("aether"));
+				
 		event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("c_ore"));
+		
+		event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("basic_core"));
+		
+		event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("weak_core"));
 
 	}
 
