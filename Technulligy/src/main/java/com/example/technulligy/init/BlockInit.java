371925bacbf33398da31a9684924ffc852bb2a67
Johnlmcint <60750709+Johnlmcint@users.noreply.github.com>
2020-07-22 13:35:49 -0700

    Started Working on some custom Blocks
    Integrating the Core Forge

--- Technulligy/src/main/java/com/example/technulligy/init/BlockInit.java
+++ Technulligy/src/main/java/com/example/technulligy/init/BlockInit.java
@@ -2,6 +2,7 @@ package com.example.technulligy.init;
 
 import com.example.technulligy.Technulligy;
 
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
@@ -20,16 +21,19 @@ import net.minecraftforge.registries.ObjectHolder;
 public class BlockInit {
 	public static final Block aether_ore = null;
 	public static final Block c_oreb = null;
+	public static final Block core_forge = null;
 
 	@SubscribeEvent
 	public static void registerBlocks(final RegistryEvent.Register<Block> event) {
 		event.getRegistry().register(new Block(Block.Properties.create(Material.IRON).hardnessAndResistance(3, 15).sound(SoundType.METAL).harvestLevel(1).harvestTool(ToolType.PICKAXE)).setRegistryName("aether_ore"));
 		event.getRegistry().register(new Block(Block.Properties.create(Material.IRON).hardnessAndResistance(15, 15).sound(SoundType.METAL).harvestLevel(1).harvestTool(ToolType.PICKAXE)).setRegistryName("c_oreb"));
+		event.getRegistry().register(new Block(AbstractBlock.Properties.create(Material.IRON).hardnessAndResistance(1, 15).sound(SoundType.METAL).harvestLevel(1).harvestTool(ToolType.PICKAXE)).setRegistryName("core_forge"));
 	}
 	@SubscribeEvent
 	public static void registerBlockItems(final RegistryEvent.Register<Item> event) {
 		event.getRegistry().register(new BlockItem(aether_ore,new Item.Properties().group(ItemGroup.MISC)).setRegistryName("aether_ore"));
 		event.getRegistry().register(new BlockItem(c_oreb, new Item.Properties().group(ItemGroup.MISC)).setRegistryName("c_oreb"));
+		event.getRegistry().register(new BlockItem(core_forge, new Item.Properties().group(ItemGroup.MISC)).setRegistryName("core_forge"));
 		
 	}
 }
