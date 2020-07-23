1ee667d3df89e17c1581c388ed63807ca176c54b
Kyle <kylechess8@gmail.com>
2020-07-23 08:56:06 -0700

    Added Deferred Registry for all items

--- Technulligy/src/main/java/com/example/technulligy/setup/Registration.java
+++ Technulligy/src/main/java/com/example/technulligy/setup/Registration.java
@@ -7,17 +7,6 @@ import net.minecraft.item.Item;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
-/*
- 	public static Item aether = null;	
-	public static Item c_ore = null;
-	public static Item basic_core = null;
-	public static Item weak_core = null;
-	public static Item savage_core = null;
-	public static Item guard_core = null;
-	public static Item wind_core = null;
-	public static Item vital_core = null;
-	public static Item haste_core = null;
- */
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class Registration {
@@ -30,7 +19,14 @@ public class Registration {
 		ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
 	}
 
-	public static final RegistryObject<Item> NULLCORE = ITEMS.register("null_core", () -> new Item(new Item.Properties().group(ItemGroup.MISC)));
-	public static final RegistryObject<Item> C = ITEMS.register("null_core", () -> new Item(new Item.Properties().group(ItemGroup.MISC)));
+	public static final RegistryObject<Item> NULL_CORE = ITEMS.register("null_core", () -> new Item(new Item.Properties().group(ItemGroup.MISC)));
+	public static final RegistryObject<Item> C_ORE = ITEMS.register("c_ore", () -> new Item(new Item.Properties().group(ItemGroup.MISC)));
+	public static final RegistryObject<Item> BASIC_CORE = ITEMS.register("basic_core", () -> new Item(new Item.Properties().group(ItemGroup.MISC)));
+	public static final RegistryObject<Item> WEAK_CORE = ITEMS.register("weak_core", () -> new Item(new Item.Properties().group(ItemGroup.MISC)));
+	public static final RegistryObject<Item> SAVAGE_CORE = ITEMS.register("savage_core", () -> new Item(new Item.Properties().group(ItemGroup.MISC)));
+	public static final RegistryObject<Item> GUARD_CORE = ITEMS.register("guard_core", () -> new Item(new Item.Properties().group(ItemGroup.MISC)));
+	public static final RegistryObject<Item> WIND_CORE= ITEMS.register("wind_core", () -> new Item(new Item.Properties().group(ItemGroup.MISC)));
+	public static final RegistryObject<Item> VITAL_CORE = ITEMS.register("vital_core", () -> new Item(new Item.Properties().group(ItemGroup.MISC)));
+	public static final RegistryObject<Item> HASTE_CORE = ITEMS.register("haste_core", () -> new Item(new Item.Properties().group(ItemGroup.MISC)));
 
 }
