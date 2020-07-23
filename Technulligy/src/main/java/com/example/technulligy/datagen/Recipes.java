03ec8bb0f440a05b12d9a292161c01296711e0da
Kyle <kylechess8@gmail.com>
2020-07-23 14:03:49 -0700

    Created First recipe using datagen

--- Technulligy/src/main/java/com/example/technulligy/datagen/Recipes.java
+++ Technulligy/src/main/java/com/example/technulligy/datagen/Recipes.java
@@ -11,6 +11,7 @@ import net.minecraft.data.IFinishedRecipe;
 import net.minecraft.data.RecipeProvider;
 import net.minecraft.data.ShapedRecipeBuilder;
 import net.minecraftforge.common.Tags;
+import net.minecraft.tags.ItemTags;
 
 public class Recipes extends RecipeProvider {
 	public Recipes(DataGenerator generatorIn) {
@@ -24,7 +25,7 @@ public class Recipes extends RecipeProvider {
 	                .patternLine("axa")
 	                .key('a', Registration.NULL_CORE.get())
 	                .key('x', Tags.Items.STONE)
-	                .key('#', Tags.Items.ORES_COAL)
+	                .key('#', ItemTags.COALS)
 	                .setGroup("technulligy")
 	                .addCriterion("stone", InventoryChangeTrigger.Instance.forItems(Blocks.STONE))
 	                .build(consumer);
