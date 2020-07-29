1ba2acab4883d0978c8d4cf61f8d8f6cf18a982b
Kyle <kylechess8@gmail.com>
2020-07-29 10:05:58 -0700

    recreating class of aethergen

--- Technulligy/src/main/java/com/example/technulligy/blocks/AetherGenerator.java
+++ Technulligy/src/main/java/com/example/technulligy/blocks/AetherGenerator.java
@@ -1,41 +1,94 @@
 package com.example.technulligy.blocks;
 
+
+
 import javax.annotation.Nullable;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.inventory.container.Container;
+import net.minecraft.inventory.container.INamedContainerProvider;
+import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.state.BooleanProperty;
-import net.minecraft.state.StateContainer.Builder;
+import net.minecraft.state.StateContainer;
 import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.World;
 import net.minecraftforge.common.ToolType;
+import net.minecraftforge.fml.network.NetworkHooks;
 
 public class AetherGenerator extends Block {
-	public static BooleanProperty POWERED = BlockStateProperties.POWERED;
+
 	public AetherGenerator() {
-	
+
 		// Properties of block
 		super(Properties.create(Material.IRON)
-				
+
 				.sound(SoundType.STONE).hardnessAndResistance(1, 15).harvestLevel(1).harvestTool(ToolType.PICKAXE)
 				.setLightLevel((p_235470_0_) -> {
 					return 14;
 				}));
-		this.setDefaultState(this.stateContainer.getBaseState().with(POWERED, false));
 	}
 
 	@Override
 	public boolean hasTileEntity(BlockState state) {
 		return true;
 	}
-@Override
-protected void fillStateContainer(Builder<Block, BlockState> builder) {
-	super.fillStateContainer(builder);
-	builder.add(POWERED);
-}
+
+	@Override
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+        builder.add(BlockStateProperties.FACING, BlockStateProperties.POWERED);
+    }
+
+	@Override
+	public int getLightValue(BlockState state, IBlockReader world, BlockPos pos) {
+		if (BlockStateProperties.POWERED.equals(true) ) {
+			return 14;
+		} else {
+			return 0;
+		}
+
+	}
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockItemUseContext context) {
+        return getDefaultState().with(BlockStateProperties.FACING, context.getNearestLookingDirection().getOpposite());
+    }
+    @Override
+    public ActionResultType onBlockActivated(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockRayTraceResult trace) {
+        if (!world.isRemote) {
+            TileEntity tileEntity = world.getTileEntity(pos);
+            if (tileEntity instanceof AetherGeneratorTile) {
+                INamedContainerProvider containerProvider = new INamedContainerProvider() {
+                    @Override
+                    public ITextComponent getDisplayName() {
+                        return new TranslationTextComponent("screen.technulligy.aether_generator");
+                    }
+
+                    @Override
+                    public Container createMenu(int i, PlayerInventory playerInventory, PlayerEntity playerEntity) {
+                        return new AetherGeneratorContainer(i, world, pos, playerInventory, playerEntity);
+                    }
+                };
+                NetworkHooks.openGui((ServerPlayerEntity) player, containerProvider, tileEntity.getPos());
+            } else {
+                throw new IllegalStateException("Our named container provider is missing!");
+            }
+        }
+        return ActionResultType.SUCCESS;
+    }
 
 	@Nullable
 	@Override
