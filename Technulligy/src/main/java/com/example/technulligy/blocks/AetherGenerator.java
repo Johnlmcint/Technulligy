8e7797eae588c3296d88f224d6602359cf6b1123
Kyle <kylechess8@gmail.com>
2020-08-03 14:05:44 -0700

    Added Tooltip

--- Technulligy/src/main/java/com/example/technulligy/blocks/AetherGenerator.java
+++ Technulligy/src/main/java/com/example/technulligy/blocks/AetherGenerator.java
@@ -1,19 +1,29 @@
 package com.example.technulligy.blocks;
 
-
+import java.util.List;
 
 import javax.annotation.Nullable;
+
+import org.lwjgl.glfw.GLFW;
+
+import com.example.technulligy.Technulligy;
+
+import net.java.games.input.Keyboard;
 //imports the block and it's mechanics
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.inventory.container.Container;
 import net.minecraft.inventory.container.INamedContainerProvider;
 import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.item.ItemStack;
 import net.minecraft.state.StateContainer;
 import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.tileentity.TileEntity;
@@ -22,6 +32,7 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
@@ -47,47 +58,60 @@ public class AetherGenerator extends Block {
 	}
 
 	@Override
-    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
-        builder.add(BlockStateProperties.FACING, BlockStateProperties.POWERED);
-    }
+	protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+		builder.add(BlockStateProperties.FACING, BlockStateProperties.POWERED);
+	}
+
+	@Override
+	public void addInformation(ItemStack stack, @Nullable IBlockReader reader, List<ITextComponent> list,
+			ITooltipFlag flags) {
+		if (Screen.hasShiftDown()) {
+			list.add(new TranslationTextComponent("message.aethergenerator", Integer.toString(1000)));
+		} else {
+		     list.add(new TranslationTextComponent("message.shiftmessage"));
+		}
+	}
 
 	@Override
 	public int getLightValue(BlockState state, IBlockReader world, BlockPos pos) {
-		if (BlockStateProperties.POWERED.equals(true) ) {
+		if (BlockStateProperties.POWERED.equals(true)) {
 			return 14;
 		} else {
 			return 0;
 		}
 
 	}
-    @Nullable
-    @Override
-    public BlockState getStateForPlacement(BlockItemUseContext context) {
-        return getDefaultState().with(BlockStateProperties.FACING, context.getNearestLookingDirection().getOpposite());
-    }
-    @Override
-    public ActionResultType onBlockActivated(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockRayTraceResult trace) {
-        if (!world.isRemote) {
-            TileEntity tileEntity = world.getTileEntity(pos);
-            if (tileEntity instanceof AetherGeneratorTile) {
-                INamedContainerProvider containerProvider = new INamedContainerProvider() {
-                    @Override
-                    public ITextComponent getDisplayName() {
-                        return new TranslationTextComponent("screen.technulligy.aether_generator");
-                    }
 
-                    @Override
-                    public Container createMenu(int i, PlayerInventory playerInventory, PlayerEntity playerEntity) {
-                        return new AetherGeneratorContainer(i, world, pos, playerInventory, playerEntity);
-                    }
-                };
-                NetworkHooks.openGui((ServerPlayerEntity) player, containerProvider, tileEntity.getPos());
-            } else {
-                throw new IllegalStateException("Oops.");
-            }
-        }
-        return ActionResultType.SUCCESS;
-    }
+	@Nullable
+	@Override
+	public BlockState getStateForPlacement(BlockItemUseContext context) {
+		return getDefaultState().with(BlockStateProperties.FACING, context.getNearestLookingDirection().getOpposite());
+	}
+
+	@Override
+	public ActionResultType onBlockActivated(BlockState state, World world, BlockPos pos, PlayerEntity player,
+			Hand hand, BlockRayTraceResult trace) {
+		if (!world.isRemote) {
+			TileEntity tileEntity = world.getTileEntity(pos);
+			if (tileEntity instanceof AetherGeneratorTile) {
+				INamedContainerProvider containerProvider = new INamedContainerProvider() {
+					@Override
+					public ITextComponent getDisplayName() {
+						return new TranslationTextComponent("screen.technulligy.aether_generator");
+					}
+
+					@Override
+					public Container createMenu(int i, PlayerInventory playerInventory, PlayerEntity playerEntity) {
+						return new AetherGeneratorContainer(i, world, pos, playerInventory, playerEntity);
+					}
+				};
+				NetworkHooks.openGui((ServerPlayerEntity) player, containerProvider, tileEntity.getPos());
+			} else {
+				throw new IllegalStateException("Oops.");
+			}
+		}
+		return ActionResultType.SUCCESS;
+	}
 
 	@Nullable
 	@Override
