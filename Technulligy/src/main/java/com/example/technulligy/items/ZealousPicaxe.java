de8dec66e9a938715eda76abd52452a4f4118a53
Kyle <kylechess8@gmail.com>
2020-08-05 13:48:05 -0700

    Revert "Revert "Revert "fake"""
    This reverts commit ecd6cfa6791da7d43e00b988d7f732f3e8e40873.

--- Technulligy/src/main/java/com/example/technulligy/items/ZealousPicaxe.java
+++ Technulligy/src/main/java/com/example/technulligy/items/ZealousPicaxe.java
@@ -3,7 +3,6 @@ package com.example.technulligy.items;
 import com.example.technulligy.Technulligy.CreativeItemGroup;
 import com.example.technulligy.setup.CoreTier;
 
-import net.minecraft.item.IItemTier;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.PickaxeItem;
@@ -11,14 +10,11 @@ import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
 
-public class ZealousPickaxe extends PickaxeItem {
+public class ZealousPicaxe extends PickaxeItem {
 
-
-	public ZealousPickaxe(IItemTier tier, int attackDamageIn, float attackSpeedIn, Properties builder) {
-		super(tier, attackDamageIn, attackSpeedIn, builder);
-
+	public ZealousPicaxe(CoreTier tier, int attackDamageIn, float attackSpeedIn, Properties builder) {
+		super(tier, 1, -2.8f, new Item.Properties().maxStackSize(1).group(CreativeItemGroup.instance));
 	}
-
 	@Override
 	public ITextComponent getDisplayName(ItemStack stack) {
 		return new TranslationTextComponent(this.getTranslationKey(stack)).mergeStyle(TextFormatting.AQUA);
