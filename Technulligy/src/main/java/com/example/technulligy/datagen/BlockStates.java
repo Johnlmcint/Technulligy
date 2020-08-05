9f08da5b69ba77b6ff17da7ad07bade1f2cfed74
Appl313245 <67352861+Appl313245@users.noreply.github.com>
2020-08-05 13:00:51 -0700

    Revert

--- Technulligy/src/main/java/com/example/technulligy/datagen/BlockStates.java
+++ Technulligy/src/main/java/com/example/technulligy/datagen/BlockStates.java
@@ -6,12 +6,9 @@ import net.minecraft.data.BlockStateProvider;
 import net.minecraft.data.DataGenerator;
 import net.minecraftforge.client.model.generators.ExistingFileHelper;
 
-public class BlockStates extends BlockStateProvider {
+public class BlockStates  {
 
-	public BlockStates(DataGenerator gen) {
-		super(gen);
-	}
-
-	
-
+	/*public BlockStates(DataGenerator gen, ExistingFileHelper exFileHelper) {
+		super(gen, Technulligy.MOD_ID, exFileHelper);
+	}*/
 }
